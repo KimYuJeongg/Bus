45946ce5db0caaef16c04915c0b33b1bd689d1a6
KimYuJeongg <dbwjd201166@gmail.com>
2021-12-14 16:01:43 +0900

    Remove fab imports

--- app/src/main/java/com/example/bus/SearchActivity.java
+++ app/src/main/java/com/example/bus/SearchActivity.java
@@ -1,15 +1,12 @@
 package com.example.bus;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.bus.databinding.ActivitySearchBinding;
 import com.example.bus.ui.main.SectionsPagerAdapter;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 public class SearchActivity extends AppCompatActivity {
