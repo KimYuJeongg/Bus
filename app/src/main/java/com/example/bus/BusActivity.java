d0ce40c1fc5f48ba442ca72a49fdc1aad82d8c95
KimYuJeongg <dbwjd2011@naver.com>
2022-03-06 23:52:40 +0900

    Add bus route data parse

--- app/src/main/java/com/example/bus/BusActivity.java
+++ app/src/main/java/com/example/bus/BusActivity.java
@@ -1,34 +1,100 @@
 package com.example.bus;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bus.adapter.BusRouteRecyclerViewAdapter;
 import com.example.bus.data.RetrofitClient;
-import com.example.bus.data.busarrival.BusArrivalInterface;
+import com.example.bus.data.busroute.BusRouteExample;
 import com.example.bus.data.busroute.BusRouteInterface;
+import com.example.bus.data.busroute.BusRouteItems;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class BusActivity extends AppCompatActivity {
+
+    TextView busStartStop, busEndStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus);
 
+        final String API_KEY = BuildConfig.API_KEY;
+
         TextView busName = findViewById(R.id.busName_activity);
         TextView busArea = findViewById(R.id.busArea_activity);
         TextView busType = findViewById(R.id.busType_activity);
-        TextView busStartStop = findViewById(R.id.busStartStop);
-        TextView busEndStop = findViewById(R.id.busEndStop);
+        busStartStop = findViewById(R.id.busStartStop);
+        busEndStop = findViewById(R.id.busEndStop);
+        RecyclerView busRouteRecyclerView = findViewById(R.id.busRouteRecyclerView);
 
         Intent intent = getIntent();
-        busName.setText(intent.getStringArrayExtra("busRouteData")[1]);
-        busType.setText(intent.getStringArrayExtra("busRouteData")[2]);
-        busStartStop.setText(intent.getStringArrayExtra("busRouteData")[3]);
-        busEndStop.setText(intent.getStringArrayExtra("busRouteData")[4]);
+        String[] busRouteData = intent.getStringArrayExtra("busRouteData");
+        busName.setText(busRouteData[1]);
+        busType.setText(busRouteData[2]);
+        busStartStop.setText(busRouteData[3]);
+        busEndStop.setText(busRouteData[4]);
         RetrofitClient retrofitClient = RetrofitClient.getInstance();
         BusRouteInterface busRouteInterface = retrofitClient.getBusRouteRetrofitInterface();
+
+        busRouteInterface.getBusStop(API_KEY, 1, "json", 25, busRouteData[0]).enqueue(new Callback<BusRouteExample>() {
+            @Override
+            public void onResponse(Call<BusRouteExample> call, Response<BusRouteExample> response) {
+                if(response.isSuccessful()) {
+                    BusRouteExample example = response.body();
+                    int totalCount = example.getResult().getBody().getTotalCount();
+                    if(totalCount > 1) {
+                        busRouteInterface.getBusStop(API_KEY, totalCount, "json", 25, busRouteData[0]).enqueue(new Callback<BusRouteExample>() {
+                            @Override
+                            public void onResponse(Call<BusRouteExample> call, Response<BusRouteExample> response) {
+                                BusRouteExample example = response.body();
+                                BusRouteItems items = example.getResult().getBody().getItems();
+                                BusRouteRecyclerViewAdapter adapter = new BusRouteRecyclerViewAdapter(items.getBusRouteItem(), true);
+                                busRouteRecyclerView.setAdapter(adapter);
+                                busRouteRecyclerView.addItemDecoration(new DividerItemDecoration(busRouteRecyclerView.getContext(), 1));
+                            }
+
+                            @Override
+                            public void onFailure(Call<BusRouteExample> call, Throwable t) {
+
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BusRouteExample> call, Throwable t) {
+
+            }
+        });
+
     }
+
+    public void upBus(View view) {
+        busStartStop.setTextColor(Color.parseColor("#01C8C1"));
+        busEndStop.setTextColor(Color.parseColor("#808080"));
+
+        BusRouteRecyclerViewAdapter adapter = new BusRouteRecyclerViewAdapter();
+        adapter.changeUpDown(true);
+    }
+
+    public void downBus(View view) {
+        busStartStop.setTextColor(Color.parseColor("#808080"));
+        busEndStop.setTextColor(Color.parseColor("#01C8C1"));
+
+        BusRouteRecyclerViewAdapter adapter = new BusRouteRecyclerViewAdapter();
+        adapter.changeUpDown(false);
+    }
+
 }
---
+++ app/src/main/java/com/example/bus/adapter/BusRouteRecyclerViewAdapter.java
@@ -0,0 +1,90 @@
+package com.example.bus.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bus.R;
+import com.example.bus.data.busroute.BusRouteItem;
+
+import java.util.List;
+
+public class BusRouteRecyclerViewAdapter extends RecyclerView.Adapter<BusRouteRecyclerViewAdapter.ViewHolder>{
+
+    List<BusRouteItem> upItems;
+    List<BusRouteItem> downItems;
+    boolean isUpDown;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView busStopName, busStopId;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            busStopName = itemView.findViewById(R.id.busStopNameRoute);
+            busStopId = itemView.findViewById(R.id.busStopIdRoute);
+        }
+
+        public void setItem(BusRouteItem item) {
+            busStopName.setText(item.getNodenm());
+            if(item.getNodeno() != null) {
+                busStopId.setText(item.getNodeno().toString());
+            }
+        }
+    }
+
+    public BusRouteRecyclerViewAdapter() {}
+
+    public BusRouteRecyclerViewAdapter(List<BusRouteItem> items, boolean isUpDown) {
+        this.isUpDown = isUpDown;
+        for(int i=0; i<items.size(); i++) {
+            if(items.get(i).getUpdowncd() == 0) {
+                upItems.add(items.get(i));
+            } else {
+                downItems.add(items.get(i));
+            }
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.bus_route_item, parent, false);
+        BusRouteRecyclerViewAdapter.ViewHolder viewHolder = new BusRouteRecyclerViewAdapter.ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if(isUpDown) {
+            holder.setItem(upItems.get(position));
+        } else {
+            holder.setItem(downItems.get(position));
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(isUpDown) {
+            return upItems.size();
+        } else {
+            return downItems.size();
+        }
+    }
+
+    public void changeUpDown(boolean isUpDown) {
+        this.isUpDown = isUpDown;
+        notifyDataSetChanged();
+    }
+
+}
